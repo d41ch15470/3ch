fde6c7ebcb97c090e46f355a3b4714b666eae94a
daichi <d.sato.development@gmail.com>
2021-11-15 21:17:16 +0900

    コントローラの追加
    疎通確認済み

---
+++ app-3ch-api/app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+    def index
+        render json: { action: action_name }
+    end
+
+    def create
+        render json: { action: action_name }
+    end
+
+    def show
+        render json: { action: action_name }
+    end
+
+    def update
+        render json: { action: action_name }
+    end
+
+    def destroy
+        render json: { action: action_name }
+    end
+end
