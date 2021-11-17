d29df972a53359d3c5ff3bf30d4aa91b6735a92b
daichi <d.sato.development@gmail.com>
2021-11-17 20:28:23 +0900

    corsの設定

--- app-3ch-api/config/application.rb
+++ app-3ch-api/config/application.rb
@@ -36,5 +36,15 @@ module App3chApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Permit cross orign
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "https://localhost:3000"
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :options, :head]
+      end
+    end
   end
 end
