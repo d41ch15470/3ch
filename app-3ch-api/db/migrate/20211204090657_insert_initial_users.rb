649bdb82d75f547df629e0ffa0471d1591d3bde6
daichi <d.sato.development@gmail.com>
2021-12-04 18:18:21 +0900

    テストユーザ作成

---
+++ app-3ch-api/db/migrate/20211204090657_insert_initial_users.rb
@@ -0,0 +1,6 @@
+class InsertInitialUsers < ActiveRecord::Migration[6.1]
+  def change
+    User.create({provider: "user_id", uid: "admin", user_id: "admin", user_type: "admin", encrypted_password: "$2a$12$Fi2IOWm.tYTk0YaHk9WFpOI30UB.4L6nOyiz40tgOqV8rShO.3Hfm"})
+    User.create({provider: "user_id", uid: "user", user_id: "user", user_type: "user", encrypted_password: "$2a$12$jmEbHs/YaPhptWMNArj9Kehq2gbiAUg6AamZDhMDwCQlE0vNnvkWC"})
+  end
+end
